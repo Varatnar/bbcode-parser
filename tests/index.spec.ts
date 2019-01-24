030111951cfb2b2c6e083c04497f00edbd706274
Varatnar <philippeleclerc299@gmail.com>
2019-01-24 09:42:43 -0500

    Modified test to test link (currently failing)

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,18 +1,16 @@
 import { expect } from "chai";
 import "mocha";
 import { BBCodeParser } from "../src/BBCodeParser";
-import { BBCodeTag } from "../src/BBCodeTag";
 
 describe("BBCodeParser", () => {
 
-    const data = "[url=coolUrl]thisIsMyUrl[test]plusMore[/test][/url]";
-
-    const URL = new BBCodeTag("url", (link) => {
-        console.log(link);
-    });
-
     const parser = BBCodeParser.withDefault();
 
-    parser.parse(data);
+    it("should parse link properly with default html library", () => {
+        const data1 = "[url=coolUrl]thisIsMyUrl[test]plusMore[/test][/url]";
+        const expected1 = "<a href='coolUrl'>thisIsMyUrl<test>plusMore</test></a>";
+
+        expect(parser.parse(data1)).to.equal(expected1);
+    });
 
 });
