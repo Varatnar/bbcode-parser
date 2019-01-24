341b6e011b2a548a07ae426488fea4c7b3eb5a80
Philippe Leclerc <philippeleclerc299@gmail.com>
2019-01-24 11:51:17 -0500

    html parsing code coverage for default library

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,11 +6,85 @@ describe("BBCodeParser", () => {
 
     const parser = BBCodeParser.withDefault();
 
-    it("should parse link properly with default html library", () => {
-        const data1 = "[url=coolUrl]thisIsMyUrl[test]plusMore[/test][/url]";
-        const expected1 = "<a href='coolUrl' target='_blank'>thisIsMyUrl<test>plusMore</test></a>";
+    describe("Default HTML Library", () => {
 
-        expect(parser.parse(data1)).to.equal(expected1);
+        it("should parse bold code", () => {
+            const data = "[b]content[/b]";
+            const expected = "<strong>content</strong>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse italic code", () => {
+            const data = "[i]content[/i]";
+            const expected = "<em>content</em>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse underline code", () => {
+            const data = "[u]content[/u]";
+            const expected = "<u>content</u>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse stroked code", () => {
+            const data = "[s]content[/s]";
+            const expected = "<s>content</s>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse highlight code", () => {
+            const data = "[h]content[/h]";
+            const expected = "<mark>content</mark>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse subscript code", () => {
+            const data = "[sub]content[/sub]";
+            const expected = "<sub>content</sub>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse supscript code", () => {
+            const data = "[sup]content[/sup]";
+            const expected = "<sup>content</sup>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse basic script code", () => {
+            const data = "[code]content[/code]";
+            const expected = "<code>content</code>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse link code", () => {
+            const data = "[url=coolUrl]thisIsMyUrl[/url]";
+            const expected = "<a href='coolUrl' target='_blank'>thisIsMyUrl</a>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        it("should parse img code", () => {
+            const data = "[img]imgPath[/img]";
+            const expected = "<img src='imagePath'>";
+
+            expect(parser.parse(data)).to.equal(expected);
+        });
+
+        for (let i = 1; i < 7; i++) {
+            it("should parse h" + i + " code", () => {
+                const data = `[h${i}]content[/h${i}]`;
+                const expected = `<h${i}>content</h${i}>`;
+
+                expect(parser.parse(data)).to.equal(expected);
+            });
+        }
     });
-
 });
